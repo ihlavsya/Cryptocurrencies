1b51437c28ea1d8f616b7053fe757bc5706ae91c
Mariia.Liashenko <Mariia.Liashenko@dataart.com>
2023-06-19 16:51:09 +0300

    Created custom navigation bar

--- Cryptocurrencies/MainWindow.xaml.cs
+++ Cryptocurrencies/MainWindow.xaml.cs
@@ -4,23 +4,12 @@ using Cryptocurrencies.API.Exceptions;
 using Cryptocurrencies.Pages;
 using Cryptocurrencies.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Cryptocurrencies
 {
@@ -31,13 +20,16 @@ namespace Cryptocurrencies
     {
         private readonly ICryptocurrenciesService _cryptocurrenciesService;
         private readonly IMapper _mapper;
+        private readonly NavigationService _navigationService;
+        MainPage mainPage = new MainPage();
         public MainWindow(ICryptocurrenciesService cryptocurrenciesService, IMapper mapper)
         {
             _cryptocurrenciesService = cryptocurrenciesService;
             _mapper = mapper;
             InitializeComponent();
-            Main.Content = new MainPage();
+            Main.Content = mainPage;
             LoadData();
+            _navigationService = Main.NavigationService;
         }
 
         private async Task LoadData()
@@ -79,6 +71,22 @@ namespace Cryptocurrencies
             {
                 AppTheme.ChangeTheme(darkUri);
             }
+        }
+
+        private void ForwardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_navigationService.CanGoForward)
+                _navigationService.GoForward();
+            else
+                _navigationService.Navigate(mainPage);
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (_navigationService.CanGoBack)
+                _navigationService.GoBack();
+            else
+                _navigationService.Navigate(mainPage);
         }
 
         private async void searchBtn_Click(object sender, RoutedEventArgs e)
